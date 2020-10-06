969c64955d7bb12c47ec43fe4052b73237fcafda
marijn kieboom <ocmkieboom@gmail.com>
2020-10-06 13:18:44 +0200

    Test2

---
+++ NinjaStore.Data/Migrations/20201006111723_asdf.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace NinjaStore.Data.Migrations
+{
+    public partial class asdf : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
