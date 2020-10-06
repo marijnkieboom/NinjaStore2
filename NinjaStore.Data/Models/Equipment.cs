6092b3557a82e72e13dfdbc1efa8b4f0263494b6
wout <woutp66@gmail.com>
2020-10-06 10:44:56 +0200

    NuGet package fix. + migrations

--- NinjaStore.Data/Models/Equipment.cs
+++ NinjaStore.Data/Models/Equipment.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.InteropServices;
 using System.Text;
 
 namespace NinjaStore.Data.Model
@@ -16,7 +18,11 @@ namespace NinjaStore.Data.Model
 
 	public class Equipment
 	{
+		[Key]
+		public int id { get; set; }
+		[Required]
 		public int Value { get; set; }
+		[Required]
 		public Category Category { get; set; }
 		public int Strength { get; set; }
 		public int Intelligence { get; set; }
--- NinjaStore.Data/Models/Ninja.cs
+++ NinjaStore.Data/Models/Ninja.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace NinjaStore.Data.Model
 {
 	public class Ninja
 	{
+		[Key]
 		public string Name { get; set; }
+		[Required]
 		public int Gold { get; set; }
 		public List<Equipment> Inventory { get; set; }
 	}
